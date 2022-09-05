a78634e31a9aec02311bdac2944551dd91fd9199
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 01:40:00 -0700

    MoodAnasyle.cs

---
+++ MoodAnasyle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnaylserUC2
+{
+    public class MoodAnasyle
+    {
+        private const string message = "I am in Sad Mood";
+        private string testmessage = "";
+        //public string Message
+        //{ 
+        //    get { return message; } 
+        //    set { message = value; }
+        //}
+        public MoodAnasyle()
+        {
+
+        }
+        public MoodAnasyle(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.testmessage == message)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
